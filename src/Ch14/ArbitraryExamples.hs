a82b35cdb4747b57895472e71668b6b84e3fd762
Andrej Palička <andrej.palicka@gmail.com>
2018-10-27 23:16:04 +0200

    Finish Chapter 14
    Some of the examples were left out. Also,
    no solutions to excersises (yet), since they mostly
    require past work. Will get to that later, hopefully.

---
+++ src/Ch14/ArbitraryExamples.hs
@@ -0,0 +1,43 @@
+module Ch14.ArbitraryExamples where
+    import Test.QuickCheck
+
+    data Trivial = Trivial deriving (Eq, Show)
+
+    trivialGen :: Gen Trivial
+    trivialGen = return Trivial
+
+    instance Arbitrary Trivial where
+        arbitrary = trivialGen
+
+
+    trivialMain :: IO()
+    trivialMain = sample trivialGen
+
+
+    data Identity a = Identity a deriving (Eq, Show)
+
+    identityGen :: Arbitrary a => Gen (Identity a)
+    identityGen = do
+        a <- arbitrary
+        return (Identity a)
+    
+    instance Arbitrary a => Arbitrary (Identity a) where
+        arbitrary = identityGen
+    
+    identityGenInt :: Gen (Identity Int)
+    identityGenInt = identityGen
+
+    data Pair a b = Pair a b deriving (Eq, Show)
+
+    pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+    pairGen = do
+        a <- arbitrary
+        b <- arbitrary
+        return (Pair a b)
+
+    instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+        arbitrary = pairGen
+
+    
+    pairGenIntString :: Gen (Pair Int String)
+    pairGenIntString = pairGen
