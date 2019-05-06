c56b638907d50496d9a8a3d9b426947a8ca8ef84
Andrej Palička <andrej.palicka@gmail.com>
2019-05-06 20:02:13 +0200

    More parsing

--- src/Ch24/LearnParsers.hs
+++ src/Ch24/LearnParsers.hs
@@ -1,30 +1,39 @@
 module Ch24.LearnParsers where
     import Text.Trifecta
+    import Control.Applicative
 
     stop :: Parser [Char]
     stop = unexpected "stop"
 
-    one :: Parser Char
-    one = char '1'
+    one :: Parser ()
+    one = char '1' >> eof
 
     one' :: Parser [Char]
     one' = one >> stop
 
-    oneTwo = char '1' >> char '2'
+    oneTwo = char '1' >> char '2' >> eof
     oneTwo' = oneTwo >> stop
 
-    testParse p = print $ parseString p mempty "123"
+    p123 :: Parser [Char]
+    p123 = do
+        (string "123" <|> string "12" <|> string "1")
+
+    testParse :: (Show a) => String -> Parser a -> IO ()
+    testParse s p = print $ parseString p mempty s
+
+    string' :: [Char] -> Parser [Char]
+    string' str = mapM char str
 
     pNL s = putStrLn ('\n' : s)
 
     main = do
         pNL "stop:"
-        testParse stop
+        testParse "123" stop
         pNL "one:"
-        testParse one
+        testParse "123" one
         pNL "one':"
-        testParse one'
+        testParse "123" one'
         pNL "oneTwo:"
-        testParse oneTwo
+        testParse "123" oneTwo
         pNL "oneTwo':"
-        testParse oneTwo'
+        testParse "123" oneTwo'
