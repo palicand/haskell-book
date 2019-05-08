2750fa9997478500839c3928d591b42f3a452759
Andrej Palicka <andrej.palicka@gmail.com>
2019-05-08 22:10:22 +0200

    Fractions parsing

---
+++ src/Ch24/Fractions.hs
@@ -0,0 +1,27 @@
+module Ch24.Fractions where
+    import Control.Applicative
+    import Data.Ratio ((%))
+    import Text.Trifecta
+
+    badFraction = "1/0"
+    alsoBad = "10"
+    shouldWork = "1/2"
+    shouldAlsoWork = "2/1"
+
+    parseFraction :: Parser Rational
+    parseFraction = do
+        numerator <- decimal
+        _ <- char '/'
+        denominator <- decimal
+        case denominator of
+            0 -> fail "Denominator cannot be a zero"
+            _ -> return (numerator % denominator)
+        
+
+    main :: IO ()
+    main = do
+        let parseFraction' = parseString parseFraction mempty
+        print $ parseFraction' shouldWork
+        print $ parseFraction' shouldAlsoWork
+        print $ parseFraction' alsoBad
+        print $ parseFraction' badFraction
