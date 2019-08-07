9137d58ab1e0bd4ae513a411514125ed0793a012
Andrej Palicka <andrej.palicka@gmail.com>
2019-08-07 20:30:32 +0200

    Add one round of Morra

--- src/Ch26/Excersise.hs
+++ src/Ch26/Excersise.hs
@@ -2,6 +2,9 @@ module Ch26.Excersise where
 
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.Maybe
+import           Control.Monad
+import           Ch26.StateT
 import           Data.Functor.Identity
 
 rDec :: Num a => Reader a a
@@ -11,8 +14,37 @@ rShow :: Show a => ReaderT a Identity String
 rShow = reader show
 
 rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
-rPrintAndInc = do
-  a <- ask
-  lift $ putStrLn $ "Hi: " ++ show a
-  return (a + 1)
+rPrintAndInc = ask
+  >>= (\a -> (lift $ putStrLn $ "Hi: " ++ show a) >> (return (a + 1)))
 
+state :: (Monad m) => (s -> (a, s)) -> StateT s m a
+state f = StateT $ \s -> return (f s)
+
+get :: (Monad m) => StateT s m s
+get = state (\s -> (s, s))
+
+put :: (Monad m) => s -> (StateT s m ())
+put s = state $ const ((), s)
+
+sPrintInAccum :: (Num a, Show a) => StateT a IO String
+sPrintInAccum = get
+  >>= (\a -> (lift $ putStrLn $ "Hi: " ++ show a)
+       >> put (a + 1)
+       >> return (show a))
+
+isValid :: String -> Bool
+isValid v = '!' `elem` v
+
+maybeExcite :: MaybeT IO String
+maybeExcite = do
+  v <- lift getLine
+  guard $ isValid v
+  return v
+
+doExcite :: IO ()
+doExcite = do
+  putStrLn "say something excite!"
+  excite <- runMaybeT maybeExcite
+  case excite of
+    Nothing -> putStrLn "MOAR EXCITE"
+    Just e  -> putStrLn ("Good, was very excite: " ++ e)
--- src/Ch26/Morra.hs
+++ src/Ch26/Morra.hs
@@ -1,26 +1,69 @@
 module Ch26.Morra where
 
 import           Control.Monad.Trans.State
-import           Control.Monad
-import           Data.Map.Strict               as M
+import           System.Random
+import           Control.Monad.IO.Class
 
-type PlayerScore = Int
-data Player = Player {playerName :: String, score :: PlayerScore}
-type GameState = StateT Int IO [Player]
+data Round = Round {
+    guesses :: (Int, Int),
+    shownFingers :: Int
+}
+data GameState = GameState {
+    score :: (Int, Int),
+    rounds :: [Round]
+}
+
+type Morra = StateT GameState IO
+
+initialState :: GameState
+initialState = GameState (0, 0) []
 
 
-gameLoop :: GameState -> IO ()
-gameLoop state = do
+askAndGenerateRandomAnswer :: String -> (Int, Int) -> IO (Int, Int)
+askAndGenerateRandomAnswer prompt range = do
+    print prompt
+    playerInput <- getLine >>= return . read
+    gen         <- getStdGen
+    let (computerInput, newGen) = randomR range gen
+    setStdGen newGen
+    return (playerInput, computerInput)
 
-    
+
+getFingers :: IO (Int, Int)
+getFingers =
+    askAndGenerateRandomAnswer "Enter the amount of fingers to show: " (0, 5)
+
+
+getGuesses :: IO (Int, Int)
+getGuesses = askAndGenerateRandomAnswer "Guess the total of fingers: " (0, 10)
+
+
+evaluateGuesses :: Int -> Int -> Int -> Morra ()
+evaluateGuesses total humanGuess computerGuess = do
+    (GameState (humanPoints, computerPoints) r) <- get
+    let newScore =
+            ( if humanGuess == total then humanPoints + 1 else humanPoints
+            , if computerGuess == total then computerPoints + 1 else humanPoints
+            )
+    put (GameState newScore (Round (humanGuess, computerGuess) total : r))
+
+gameLoop :: Morra ()
+gameLoop = do
+    (playerFingers, computerFingers) <- liftIO getFingers
+    (playerGuess  , computerGuess  ) <- liftIO getGuesses
+    evaluateGuesses (playerFingers + computerFingers) playerGuess computerGuess
+    liftIO $ print ("Fingers shown: " ++ show (playerFingers + computerFingers))
+    liftIO $ print
+        ("Human guessed " ++ show (playerGuess) ++ ", computer guessed " ++ show
+            (computerGuess)
+        )
+
 
 main :: IO ()
 main = do
-    print "Enter first players name: "
-    firstPlayerName <- readLn
-    print "Enter second players name: "
-    secondPlayerName <- readLn
-    let firstPlayer  = Player 0 firstPlayerName
-        secondPlayer = Player 0 secondPlayerName
-        playerScores = [firstPlayer, secondPlayer]
-        forever $ gameLoop $ stateT
+    ((), GameState (humanResults, computerResults) _) <- runStateT
+        gameLoop
+        initialState
+    print ("Human: " ++ show humanResults)
+    print ("Computer: " ++ show computerResults)
+    print "Game Over"
