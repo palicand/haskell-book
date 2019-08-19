133b77fdd0b4d745b3ed336f9d0a1d321482422f
Andrej Palicka <andrej.palicka@gmail.com>
2019-08-19 18:29:44 +0200

    Extend morra

--- src/Ch26/Morra.hs
+++ src/Ch26/Morra.hs
@@ -1,48 +1,85 @@
 module Ch26.Morra where
 
 import           Control.Monad
+import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.State
+import           Control.Monad.Trans.Maybe
+import           Control.Monad.Trans.Reader
 import           System.Random
 import           Control.Monad.IO.Class
 
+
+type Fingers = Int
+type Guess = Int
+type Score = Int
+
 data Round = Round {
-    guesses :: (Int, Int),
-    shownFingers :: Int
+    guesses :: (Guess, Guess),
+    shownFingers :: Fingers
 }
 data GameState = GameState {
-    score :: (Int, Int),
+    score :: (Score, Score),
     rounds :: [Round]
 }
 
-type Morra = StateT GameState IO
+
+data GameMode = PvAI | PvP
+
+type GameReader = ReaderT GameMode IO
+
+type Morra = StateT GameState GameReader
+
 
 initialState :: GameState
 initialState = GameState (0, 0) []
 
 
-winningScore :: Int
+winningScore :: Score
 winningScore = 3
 
-askAndGenerateRandomAnswer :: String -> (Int, Int) -> IO (Int, Int)
-askAndGenerateRandomAnswer prompt range = do
+
+askFor :: (Read a) => String -> IO a
+askFor prompt = do
     print prompt
-    playerInput <- getLine >>= return . read
-    gen         <- getStdGen
+    getLine >>= return . read
+
+generateRandomAnswer :: (Random a) => (a, a) -> IO a
+generateRandomAnswer range = do
+    gen <- getStdGen
     let (computerInput, newGen) = randomR range gen
     setStdGen newGen
-    return (playerInput, computerInput)
+    return computerInput
 
 
-getFingers :: IO (Int, Int)
-getFingers =
-    askAndGenerateRandomAnswer "Enter the amount of fingers to show: " (0, 5)
+getSecondPlayerAnswer
+    :: (Random a, Read a) => GameMode -> String -> (a, a) -> IO a
+getSecondPlayerAnswer PvAI _      range = generateRandomAnswer range
+getSecondPlayerAnswer PvP  prompt _     = askFor prompt
 
 
-getGuesses :: IO (Int, Int)
-getGuesses = askAndGenerateRandomAnswer "Guess the total of fingers: " (0, 10)
+getFingers :: GameReader (Fingers, Fingers)
+getFingers = do
+    let prompt = "How many fingers do you want to show: "
+    player1Guess <- liftIO $ askFor prompt
+    mode         <- ask
+    player2Guess <- liftIO $ getSecondPlayerAnswer mode prompt (0, 5)
+    return (player1Guess, player2Guess)
 
 
-evaluateGuesses :: Int -> Int -> Int -> GameState -> GameState
+
+getGuesses :: Fingers -> GameReader (Guess, Guess)
+getGuesses computerFingers = do
+    let prompt = "Enter your guess:"
+    player1Guess <- liftIO $ askFor prompt
+    mode         <- ask
+    player2Guess <- liftIO $ getSecondPlayerAnswer
+        mode
+        prompt
+        (computerFingers, computerFingers + 5)
+    return (player1Guess, player2Guess)
+
+
+evaluateGuesses :: Fingers -> Guess -> Guess -> GameState -> GameState
 evaluateGuesses total humanGuess computerGuess (GameState (humanPoints, computerPoints) r)
     = GameState newScore (Round (humanGuess, computerGuess) total : r)  where
     newScore =
@@ -53,31 +90,51 @@ evaluateGuesses total humanGuess computerGuess (GameState (humanPoints, computer
 
 gameLoop :: Morra ()
 gameLoop = do
-    (playerFingers, computerFingers) <- liftIO getFingers
-    (playerGuess  , computerGuess  ) <- liftIO getGuesses
+    (playerFingers, computerFingers) <- lift getFingers
+    (playerGuess  , computerGuess  ) <- lift $ getGuesses computerFingers
     oldState                         <- get
     let newState = evaluateGuesses (playerFingers + computerFingers)
                                    playerGuess
                                    computerGuess
                                    oldState
     put newState
-    liftIO $ print ("Fingers shown: " ++ show (playerFingers + computerFingers))
-    liftIO $ print
+    liftIO $ putStrLn
+        ("Fingers shown: " ++ show (playerFingers + computerFingers))
+    liftIO $ putStrLn
         (  "Human guessed "
         ++ show playerGuess
         ++ ", computer guessed "
         ++ show computerGuess
         )
     let (humanScore, computerScore) = score newState
+    liftIO $ putStrLn ("Human: " ++ show humanScore)
+    liftIO $ putStrLn ("Computer: " ++ show computerScore)
     unless (humanScore == winningScore || computerScore == winningScore)
            gameLoop
 
 
-main :: IO ()
-main = do
-    ((), GameState (humanResults, computerResults) _) <- runStateT
-        gameLoop
-        initialState
-    print ("Human: " ++ show humanResults)
-    print ("Computer: " ++ show computerResults)
-    print "Game Over"
+askForGameMode :: IO (Maybe GameMode)
+askForGameMode = do
+    liftIO $ putStrLn
+        "Enter 1 to play against AI or 2 to play against another player"
+    opt <- getLine >>= return . read
+    case opt of
+        "1" -> return $ Just PvAI
+        "2" -> return $ Just PvP
+        _   -> return Nothing
+
+
+morra :: IO ()
+morra = do
+    gameMode <- askForGameMode
+    case gameMode of
+        Nothing     -> putStrLn "Incorrect game mode"
+        (Just mode) -> do
+            ((), GameState (humanResults, computerResults) _) <- runReaderT
+                (runStateT gameLoop initialState)
+                mode
+            case compare humanResults computerResults of
+                EQ -> putStrLn "Draw!"
+                LT -> putStrLn "The computer wins!"
+                GT -> putStrLn "Congratulation, human wins!"
+            putStrLn "Game Over"
